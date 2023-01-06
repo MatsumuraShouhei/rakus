ec32f21aa0009336b17696ec7778d931b0a33303
3030558 <3030558@30305-58>
2023-01-06 14:50:01 +0900

    aaaaaaa

--- src/rakustest/Aaa.java
+++ src/rakustest/Aaa.java
@@ -1,6 +1,20 @@
 package rakustest;
 
-public class Aaa {
+import java.awt.BorderLayout;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JTextArea;
+
+public class Aaa extends JFrame{
+	public Aaa() {
+		
+		JButton btnNewButton = new JButton("New button");
+		getContentPane().add(btnNewButton, BorderLayout.WEST);
+		
+		JTextArea textArea = new JTextArea();
+		getContentPane().add(textArea, BorderLayout.CENTER);
+	}
 	
 	public static void main(String[] args) {
 		
